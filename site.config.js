2c6332b38d9346d4ba32110e82524ba92e94b785
yeji-l <lyj990701@naver.com>
2024-02-21 15:05:22 +0900

    update profile

--- site.config.js
+++ site.config.js
@@ -1,9 +1,9 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "yeji",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
-    role: "frontend developer",
+    role: "UI Designer",
     bio: "I develop everything using node.",
     email: "morethanmin.dev@gmail.com",
     linkedin: "morethanmin",
